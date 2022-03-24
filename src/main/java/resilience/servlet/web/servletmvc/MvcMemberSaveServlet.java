081322bfd19929cc0c42462794ffbb8d05e6aa3a
hayden <soar0706@gmail.com>
2022-03-24 23:03:29 +0900

    servlet mvc

---
+++ src/main/java/resilience/servlet/web/servletmvc/MvcMemberSaveServlet.java
@@ -0,0 +1,35 @@
+package resilience.servlet.web.servletmvc;
+
+import resilience.servlet.domain.member.Member;
+import resilience.servlet.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
+public class MvcMemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+
+        String userName = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(userName, age);
+
+        memberRepository.save(member);
+
+        req.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+}
